adabfb35382db0806dbdebd95d1ad4c5ac9bd3b8
Rafael Furlanetto <rafael.furlanetto@uel.br>
2023-12-04 13:03:33 -0300

    Fixing Build after PyInstall conflict with pydicom

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ cd release
 rmdir /s release-win
 md release-windows_64x
 cd release-windows_64x
-python -m PyInstaller --noconsole --onefile --windowed ../../main.py --name ContExt --icon ../../icons/Icon.ico
+python -m PyInstaller -F --collect-submodules=pydicom --noconsole --onefile --windowed ../../main.py --name ContExt --icon ../../icons/Icon.ico
 cd dist
 Xcopy .\..\..\..\icons .\icons /E /H /C /I
 Xcopy .\..\..\..\fonts .\fonts /E /H /C /I
--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ cd release
 rm -rf release-linux_64x
 mkdir release-linux_64x
 cd release-linux_64x
-pyinstaller --onefile ../../main.py --name ContExt --icon ../../icons/Icon.ico
+pyinstaller --onefile -F --collect-submodules=pydicom ../../main.py --name ContExt --icon ../../icons/Icon.ico
 cd ..
 cd ..
